07baa6e7427a5794c131acda612b86bbb999b16a
Gopal Jha <gopaljha9398715741@gmail.com>
2025-10-18 23:04:13 +0530

    somefixex

--- backend/src/controllers/friendsController.js
+++ backend/src/controllers/friendsController.js
@@ -30,27 +30,27 @@ const getAllRequests = async (req, res) => {
   }
 };
 
-const sendRequest = async (req , res) =>{
-   try{
-     
+const sendRequest = async (req, res) => {
+  try {
+
     const user = req.user;
 
-    if(!user){
-        return res.status(404).json({
-            success:false,
-            message:"User doesn't exists"
-        })
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User doesn't exists"
+      })
     }
 
     //find user by email , name , number which is in the database
-    const {email , phoneNumber , name} = req.body;
-    if(!email && !phoneNumber && !name){
-        return res.status(404).json({
-            success:false,
-            message:"Please provide email , phoneNumber or name to search"
-        })
+    const { email, phoneNumber, name } = req.body;
+    if (!email && !phoneNumber && !name) {
+      return res.status(404).json({
+        success: false,
+        message: "Please provide email , phoneNumber or name to search"
+      })
     }
-    
+
 
     const recipient = await User.findOne({
       $or: [
@@ -83,7 +83,7 @@ const sendRequest = async (req , res) =>{
     }
 
     //areadyy request sent
-     
+
     const existingRequest = recipient.friendRequests.find(
       req => req.sender.equals(user._id) && req.status === "pending"
     );
@@ -95,7 +95,7 @@ const sendRequest = async (req , res) =>{
     }
 
     ///send request
-     recipient.friendRequests.push({
+    recipient.friendRequests.push({
       sender: user._id,
       status: "pending",
       sentAt: Date.now(),
@@ -104,23 +104,66 @@ const sendRequest = async (req , res) =>{
 
 
     return res.status(200).json({
-        success:true,
-        message:"Friend request sent"
+      success: true,
+      message: "Friend request sent"
     })
 
-   }catch(err){
+  } catch (err) {
     return res.status(500).json({
-        success:false,
-        message:err.message
+      success: false,
+      message: err.message
     })
-   }
+  }
+}
+
+const acceptRequest = async (req, res) => {
+  try {
+    const { user } = req.user;
+    const { senderId } = req.body;
+
+    if (!user) {
+      return res.status(404).json({
+        message: "Authenticate first"
+      })
+    }
+
+    const request = user.friendRequests.findIndex((r) => r.sender.toString() === senderId && r.status === "pending");
+
+    if (request == -1) {
+      return res.status(400).json({ success: false, message: "No pending request found" });
+    }
+
+    user.friendRequests[request].status = "accepted";
+
+    user.friends.push(senderId);
+
+    const sender = await User.findById(senderId);
+    sender.friends.push(user._id);
+
+    await sender.save();
+    await user.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Friend request accepted successfully",
+    });
+
+  } catch (err) {
+      return res.status(404).json({
+        message:"Unable to added the friends Please try again later",
+        success:false,
+        err:err.message
+      })
+  }
 }
 
 
 
 
+
+
 module.exports = {
-    sendRequest,
-    getAllRequests
+  sendRequest,
+  getAllRequests
 }
 
